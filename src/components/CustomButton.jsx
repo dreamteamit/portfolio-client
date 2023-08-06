16671e1e0dc2492c5d1a6418a1197fbaebad5415
Nizam Shejan <nizam.shejan11@gmail.com>
2023-08-06 20:56:23 +0600

    footer added

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 const CustomButton = ({ children }) => {
   return (
-    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">
+    <button
+      type="button"
+      className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+    >
       {children}
     </button>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  daisyui: {
-    themes: [
-      {
-        doctortheme: {
-          primary: "#a51890",
-          secondary: "#013ca6",
-          accent: "#3A4256",
-          neutral: "#3D4451",
-          "base-100": "#FFFFFF",
-        },
-      },
-    ],
-  },
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-};
-=======
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  
-  plugins: [
-    require('flowbite/plugin')
-]
-}
 
->>>>>>> 8f20f057d483e6cd84db3cce514b172c6a1405f9
+  plugins: [require("flowbite/plugin")],
+};
