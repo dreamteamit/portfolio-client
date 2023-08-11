89c9f1a77699b19b44bed58cbff831ec1ae9981e
Jonaeath <jonaeathbcc18@gmail.com>
2023-08-11 23:15:20 +0600

    mycommittwo

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,15 +1,24 @@
+import { Button } from 'flowbite-react';
+import image from '../../assets/slider/about_11.jpg';
+import '../About/About.css';
 
 const About = () => {
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
-                    </div>
+        <div className='aboutMain'>
+            <div className='aboutImage'>
+                <img src={image} alt="" />
+            </div>
+            <div className='aboutOne'>
+                <h4 className='text-blue-500 font-bold mb-5'>WHO WE ARE ---</h4>
+                <h1 className='text-5xl font-bold mb-8'>We Can Take Your Business<br />To The <span className='text-blue-500'>Next Level.</span> </h1>
+                <p className='mb-9 p-1'>It is a long established fact that a reader will be distracted by the <br /> readable content of a page when looking at its layout. Lorem Ipsum is<br /> simply dummy text of the printing and typesetting industry.</p>
+                <div className='font-bold text-xl pb-2'>
+                    <p><span className='text-blue-500'>&bull; </span>we are always ensure to give our best for our customer.</p>
+                    <p><span className='text-blue-500'>&bull; </span> We give professional support by our professional.</p>
+                    <p><span className='text-blue-500'>&bull; </span>Our mission is to provide quality product design.</p>
+                </div>
+                <div className='mt-16'>
+                    <Button id="buttoncol">Get Started Now</Button>
                 </div>
             </div>
         </div>
