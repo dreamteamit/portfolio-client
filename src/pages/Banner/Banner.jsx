5e0052e586283e5bb8f1fb8b72cdd92d20e9297c
Md. Rishad Islam <rishad.islam158@gmail.com>
2023-08-12 21:43:18 +0600

    menu update

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -5,76 +5,139 @@ const Banner = () => {
     <div>
       <div className="relative">
         {/* menu */}
-        <div className="bg-transparent absolute top-10 z-50 text-white w-full">
-          <div className="md:flex md:justify-around md:items-center">
-
-            {/* logo */}
-            <div>
-              <h1>Dream IT</h1>
+        <nav className="bg-transparent absolute top-10 z-50 text-white w-full">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+            <a href="https://flowbite.com/" className="flex items-center">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Dream IT
+              </span>
+            </a>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-default"
+            >
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  ">
+                <li>
+                  <Link
+                    to="home"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="service"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    Service
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="about"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="portfolio"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    Portfolio
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="portfolio"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    Testimonial
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="blog"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="contact"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
             </div>
-            {/* main menu */}
-            <div className="md:flex gap-10 text-xl">
-              <Link to="home" activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="cursor-pointer">Home</Link>
-
-              <Link to="service" activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="cursor-pointer">Service</Link>
-
-
-              <Link to="about" activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="cursor-pointer">About</Link>
-
-
-
-
-              <Link to="portfolio" activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="cursor-pointer">Portfolio</Link>
-
-
-              <Link to="portfolio" activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="cursor-pointer">Testimonial</Link>
-
-              <Link to="blog" activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="cursor-pointer">Blog</Link>
-
-              <Link to="contact" activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="cursor-pointer">Contact Us</Link>
-
-
-
-            </div>
-
           </div>
+
           <hr className="border border-gray-500 w-full mt-10" />
-        </div>
+        </nav>
         <StaticCarousel />
       </div>
     </div>
